30b5420d1022b6866811b1f27cae7f4da7372d47
Daniel Hudson <danielh@asana.com>
2018-04-04 12:58:35 -0700

    update deps, add commander, separate fn concerns, unit tests

---
+++ fixtures/fixtures.js
@@ -0,0 +1,110 @@
+const parsedCsv = [
+    {
+        "Date Joined Organization": "2018-02-19",
+        "Email Address": "member1@internal.com",
+        "First Login Date": "2015-09-21",
+        Internal: "true",
+        "Last Activity": "2018-04-02",
+        "Login Method": "Password",
+        Name: "Member1",
+        "Number of Projects": undefined,
+        "Number of Teams": "9",
+        Projects: undefined,
+        Teams: ""
+    },
+    {
+        "Date Joined Organization": "2016-06-22",
+        "Email Address": "member2@internal.com",
+        "First Login Date": "2015-09-22",
+        Internal: "true",
+        "Last Activity": "2016-02-29",
+        "Login Method": "Password",
+        Name: "Member2",
+        "Number of Projects": "34",
+        "Number of Teams": "1",
+        Projects: "",
+        Teams: ""
+    },
+    {
+        "Date Joined Organization": "2018-10-12",
+        "Email Address": "member3@internal.com",
+        "First Login Date": "2015-10-12",
+        Internal: "true",
+        "Last Activity": "2015-10-13",
+        "Login Method": "Password",
+        Name: "Member3",
+        "Number of Projects": "2",
+        "Number of Teams": "1",
+        Projects: "",
+        Teams: "test"
+    },
+    {
+        "Date Joined Organization": "2016-03-15",
+        "Email Address": "",
+        "First Login Date": "2016-03-15",
+        Internal: "true",
+        "Last Activity": "2016-04-19",
+        "Login Method": "Password",
+        Name: "Member4",
+        "Number of Projects": "1",
+        "Number of Teams": "2",
+        Projects: "",
+        Teams: ""
+    },
+    {
+        "Date Joined Organization": "2018-02-10",
+        "Email Address": "guest1@external.com",
+        "First Login Date": "2017-01-10",
+        Internal: "false",
+        "Last Activity": "2017-01-27",
+        "Login Method": "Password",
+        Name: "Guest1",
+        "Number of Projects": undefined,
+        "Number of Teams": "5",
+        Projects: undefined,
+        Teams: ""
+    },
+    {
+        "Date Joined Organization": "2017-12-25",
+        "Email Address": "guest2@external.com",
+        "First Login Date": "",
+        Internal: "false",
+        "Last Activity": "",
+        "Login Method": "",
+        Name: "Guest2",
+        "Number of Projects": "0",
+        "Number of Teams": "0",
+        Projects: "",
+        Teams: ""
+    },
+    {
+        "Date Joined Organization": "2018-01-08",
+        "Email Address": "guest3@external.com",
+        "First Login Date": "",
+        Internal: "false",
+        "Last Activity": "2018-03-08",
+        "Login Method": "Not yet logged in",
+        Name: "Guest3",
+        "Number of Projects": "0",
+        "Number of Teams": "1",
+        Projects: "",
+        Teams: "Teams!"
+    },
+    {
+        "Date Joined Organization": "2018-01-08",
+        "Email Address": "guest4@external.com",
+        "First Login Date": "",
+        Internal: "false",
+        "Last Activity": "2018-04-01",
+        "Login Method": "2017-01-10",
+        Name: "Guest4",
+        "Number of Projects": "0",
+        "Number of Teams": "1",
+        Projects: "",
+        Teams: "Teams!"
+    }
+];
+
+module.exports = {
+   parsedCsv
+};
--- index.js
+++ index.js
@@ -1,129 +1,157 @@
 const asana = require("asana");
-const Bluebird = require("bluebird");
 const csv = require("csv-parser");
 const moment = require("moment");
 const request = require("request-promise");
+const commander = require("commander");
 const fs = require("fs");
 
 const loadRemoteCsv = url => {
-  console.log("Downloading remote csv file", url);
-  return loadCsv(request(url));
+    console.log("Downloading remote csv file", url);
+    return loadCsv(request(url));
 };
 
 const loadLocalCsv = path => {
-  console.log("Reading local csv file", path);
+    console.log("Reading local csv file", path);
 
-  return loadCsv(fs.createReadStream(path));
+    return loadCsv(fs.createReadStream(path));
 };
 
 const loadCsv = stream => {
-  return new Bluebird((resolve, reject) => {
-    const rows = [];
+    return new Promise((resolve, reject) => {
+        const rows = [];
 
-    stream
-      .pipe(csv())
-      .on("data", data => {
-        rows.push(data);
-      })
-      .on("end", () => {
-        resolve(rows);
-      })
-      .on("error", err => {
-        reject(err);
-      });
-  });
+        stream
+            .pipe(csv())
+            .on("data", data => {
+                rows.push(data);
+            })
+            .on("end", () => {
+                resolve(rows);
+            })
+            .on("error", err => {
+                reject(err);
+            });
+    });
 };
 
-const createAsanaClient = () => {
-  const accessToken = process.argv[2];
-  console.log("Loading Asana API with access token", accessToken);
-  return asana.Client.create().useAccessToken(accessToken);
+const createAsanaClient = token => {
+    console.log("Loading Asana API with access token", token);
+    return asana.Client.create({}).useAccessToken(token);
 };
 
-const deprovisionFunc = function() {
-  const organizationId = process.argv[4];
-  console.log("Will deprovision from", organizationId);
-  const asanaClient = createAsanaClient();
-  return email => {
-    return asanaClient.workspaces
-      .removeUser(organizationId, {
-        user: email
-      })
-      .then(function() {
-        console.log("Successfully deprovisioned", email);
-      })
-      .catch(function(ex) {
-        console.log("Error while deprovisioning", email, ex.value.errors);
-      });
-  };
+const deprovisionFunc = (asanaClient, domain) => {
+    console.log("Will deprovision from", domain);
+    return email => {
+        return asanaClient.workspaces
+            .removeUser(domain, {
+                user: email
+            })
+            .then(() => {
+                console.log("Successfully deprovisioned", email);
+            })
+            .catch(ex => {
+                console.log("Error while deprovisioning", email, ex.value.errors);
+            });
+    };
 };
 
 /**
  * Returns guest users who haven't logged in for over a month by email address
  */
-const usersToDeprovision = csv => {
-  const threshold = moment().subtract(parseInt(process.argv[5], 10), "days");
+const usersToDeprovision = (csv, days) => {
+    const threshold = moment().subtract(days, "days");
 
-  return csv.then(function(data) {
-    const realUsers = data.filter(row => {
-      return row["Email Address"] !== "";
+    return csv.then(data => {
+        const realUsers = data.filter(row => {
+            return row["Email Address"] !== "";
+        });
+
+        const guests = realUsers.filter(row => {
+            return row["Internal"] === "false";
+        });
+
+        const rowsToDeprovision = guests.filter(row => {
+            let lastActivityMoment = moment(row["Last Activity"]);
+
+            if (!lastActivityMoment.isValid()) {
+                // Probably hasn't logged in, fall back to invitation date instead
+                lastActivityMoment = moment(row["Date Joined Organization"]);
+            }
+            return lastActivityMoment.isBefore(threshold);
+        });
+
+        return rowsToDeprovision.map(row => {
+            return row["Email Address"];
+        });
     });
-
-    const guests = realUsers.filter(row => {
-      return row["Internal"] === "false";
-    });
-
-    const rowsToDeprovision = guests.filter(row => {
-      const lastActivityMoment = moment(row["Last Activity"]);
-
-      if (!lastActivityMoment.isValid()) {
-        // Probably hasn't logged in, fall back to invitation date instead
-        lastActivityMoment = moment(row["Date Joined Organization"]);
-      }
-      return lastActivityMoment.isBefore(threshold);
-    });
-
-    return rowsToDeprovision.map(row => {
-      return row["Email Address"];
-    });
-  });
 };
 
 const main = () => {
-  if (process.argv.length < 6 || process.argv.length > 7) {
-    console.log("Usage:");
-    console.log(
-      "node index.js <service account token> <member export csv path or url> <organization id> <# of days inactive threshold> [action]"
-    );
-  }
+    // 1. Take user inputs from options
+    commander
+        .version("0.0.2")
+        .option("-a, --auth <token>", "Service account token")
+        .option("-c, --csv <path>", "Path to member csv file")
+        .option("-d, --domain <domainId>", "Organization or Workspace domain ID")
+        .option("-t, --threshold <#>", "# of days inactive threshold", parseInt)
+        .option("-m, --mode ['dry' or 'action']", "mode to run the script in")
+        .parse(process.argv);
 
-  if (process.argv[6] !== "action") {
-    console.log(
-      "In dry-run mode. Add 'action' to the command line to actually deprovision users"
-    );
-  }
-
-  const deprovision = deprovisionFunc();
-
-  // Decide whether or not this is a file path or a url
-  const csvData = process.argv[3].includes("https://")
-    ? loadRemoteCsv(process.argv[3])
-    : loadLocalCsv(process.argv[3]);
-
-  usersToDeprovision(csvData).then(emails => {
-    if (emails.length > 0) {
-      emails.forEach(email => {
-        if (process.argv[6] === "action") {
-          console.log("Deprovisioning", email);
-          deprovision(email);
-        } else {
-          console.log("Planning to deprovision", email);
-        }
-      });
-    } else {
-      console.log("No one will be deprovisioned.");
+    // 2. Validate inputs
+    if (!commander.auth) {
+        console.log("Please set the `--auth` option with a service account token");
+        return;
     }
-  });
+    if (!commander.csv) {
+        console.log("Please set the`--csv` option with a path or URL to a CSV file");
+        return;
+    }
+    if (!commander.domain) {
+        console.log("Please set a the `--domain` option with your domain ID");
+        return;
+    }
+    if (!commander.threshold) {
+        console.log(
+            "Please set a the `--threshold` option with the number of days of inactivity you'd like to count as being inactive"
+        );
+        return;
+    }
+    if (commander.mode !== "action") {
+        console.log("In dry-run mode. set --mode option to 'action' to actually deprovision users");
+    }
+
+    // 3. Process CSV
+    const csvData = commander.csv.includes("https://")
+        ? loadRemoteCsv(commander.csv)
+        : loadLocalCsv(commander.csv);
+
+    // 4. Create Asana client
+    const asanaClient = createAsanaClient(commander.auth);
+
+    // 5. Set up deprovision function
+    const deprovision = deprovisionFunc(asanaClient, commander.domain);
+
+    // 6. Deprovision users
+    usersToDeprovision(csvData, commander.threshold).then(emails => {
+        if (emails.length > 0) {
+            emails.forEach(email => {
+                if (commander.mode === "action") {
+                    console.log("Deprovisioning", email);
+                    deprovision(email);
+                } else {
+                    console.log("Planning to deprovision", email);
+                }
+            });
+        } else {
+            console.log("No one will be deprovisioned.");
+        }
+    });
 };
 
 main();
+
+module.exports = {
+    loadCsv,
+    deprovisionFunc,
+    usersToDeprovision
+};
