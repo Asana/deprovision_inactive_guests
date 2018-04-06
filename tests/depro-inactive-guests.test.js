c00d450a327b40551feeaab26a067531437fd7a6
Daniel Hudson <danielh@asana.com>
2018-04-06 13:41:59 -0700

    Move tests to "tests" directory, specify tests file in jest node script

--- tests/depro-inactive-guests.test.js
+++ tests/depro-inactive-guests.test.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-const index = require("./depro-inactive-guests");
-const fixtures = require("./fixtures/fixtures");
+const index = require("./../depro-inactive-guests");
+const fixtures = require("./../fixtures/fixtures");
 
 describe("Processes a local CSV correctly", () => {
     // setup
